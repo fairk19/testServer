1d6892a293f0c67c317ea5cc377aa8d6682bc97d
alexandr <fairk19@gmail.com>
2014-03-24 20:15:48 +0300

    server run success, not work game, add testng and cobertura

---
+++ src/main/tests/utils/SysInfoTest.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Александр on 24.03.14.
+ */
+public class SysInfoTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStat() throws Exception {
+
+    }
+}
